f19088b81567f04e4b8b2361a7a659161eca848d
Jordan <jgreene097@gmail.com>
2021-11-01 22:07:08 -0400

    Made home page prettier and added jsx files

---
+++ src/components/Cryptocurrencies.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Cryptocurrencies = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Cryptocurrencies
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
+import 'antd/dist/antd.css'
 
 // run app
 ReactDOM.render(
