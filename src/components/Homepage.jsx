977a9742f41bfbc99ddfae19b643ba55aa4ac6f6
Jordan <jgreene097@gmail.com>
2021-11-17 13:06:22 -0500

    RapidAPI has been deprecated. Waiting for migration

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,6 +3,7 @@ import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import { Cryptocurrencies, News } from '../components';
 
 
 const { Title } = Typography;
@@ -10,23 +11,32 @@ const { Title } = Typography;
 export const Homepage = () => {
     // this is a hook
     const { data, isFetching } = useGetCryptosQuery();
-    const globalStats = data?.data?.stats;
+    //const globalStats = data?.data?.stats;
 
     console.log(data);
 
-    if (isFetching) return "Loading...";
-    
+    //if (isFetching) return "Loading...";
+    //{millify(data?.data?.stats.totalMarketCap)}
 
     return (
         <>
             <Title level={2} className="heading">Global Crypto Statistics</Title>
             <Row>
-                <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total}/></Col>
-                <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)}/></Col>
-                <Col span={12}><Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)}/></Col>
-
-                <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}/></Col>
+                <Col span={12}><Statistic title="Total Cryptocurrencies" value={data?.data?.totalCoins}/></Col>
+                <Col span={12}><Statistic title="Total Exchanges" value="5"/></Col>
+                <Col span={12}><Statistic title="Total Market Cap" value="5"/></Col>
+                <Col span={12}><Statistic title="Total Markets" value="5"/></Col>
             </Row>
+            <div className="home-heading-container">
+                <Title level={2} className="home-title">Top 10 Cryptocurrencies in the world</Title>
+                <Title level={3} className="show-more"><Link to="/cyrptocurrencies">Show more</Link></Title>
+            </div>
+            <Cryptocurrencies simplified/>
+            <div className="home-heading-container">
+                <Title level={2} className="home-title">Latest Crypto News</Title>
+                <Title level={3} className="show-more"><Link to="/news">Show more</Link></Title>
+            </div>
+            <news simplified/>
 
         </>
     )
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,12 +1,31 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const cryptoApiHeaders = {
-    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-    'x-rapidapi-key': '3b5990d802mshfb88fd388819a06p1ae24ajsn588bcafbd07c'
-}
+const cryptoApiHeader = { 'x-access-token' : 'coinrankingfdbc4a2f66c216103b3011e3d8dee704e692cef8ae357f8a'}
+const baseUrl = 'https://api.coinranking.com/v2'; 
+const createRequest = (url) => ({ url, headers: cryptoApiHeader})
 
-const baseUrl = 'https://coinranking1.p.rapidapi.com';
-const createRequest = (url) => ({ url, headers: cryptoApiHeaders})
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/stats')
+        })
+    })
+});
+
+// creates hook to pull instantly to get all data for quey thanks to redux
+export const { useGetCryptosQuery } = cryptoApi;
+
+
+//////// rapidAPI has been deprecated kinda old stuff
+
+/* 
+
+const cryptoApiHeader = { 'x-access-token' : 'coinrankingfdbc4a2f66c216103b3011e3d8dee704e692cef8ae357f8a'}
+
+const baseUrl = 'https://api.coinranking.com/v2'; 
+const createRequest = (url) => ({ url, headers: cryptoApiHeader})
 
 // Api to get info from RapidApi
 export const cryptoApi = createApi({
@@ -14,10 +33,9 @@ export const cryptoApi = createApi({
     baseQuery: fetchBaseQuery({ baseUrl }),
     endpoints: (builder) => ({
         getCryptos: builder.query({
-            query: () => createRequest('/coins')
+            query: () => createRequest('/stats')
         })
     })
 });
 
-// creates hook to pull instantly to get all data for quey thanks to redux
-export const { useGetCryptosQuery } = cryptoApi;
+*/
